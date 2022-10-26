2cd7ec0a06bd23e857c0670cd660d481d2b5167c
Job Doesburg <job.doesburg@gmail.com>
2022-10-26 20:57:15 +0200

    Queryset actions

--- src/django_easy_admin_object_actions/admin.py
+++ src/django_easy_admin_object_actions/admin.py
@@ -1,3 +1,10 @@
+from django.contrib import messages
+from django.contrib.admin.utils import model_ngettext
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.utils.translation import gettext
+
+
 class ObjectActionsMixin:
     change_form_template = "easy_admin_object_actions/admin/change_form.html"
 
@@ -22,6 +29,9 @@ class ObjectActionsMixin:
                 ):
                     continue
                 action = getattr(self, action_name)
+
+            if obj is None and not getattr(action, "perform_after_saving", True):
+                continue  # no object actions before saving for new objects, as the objects don't yet exist
 
             has_permission = bool(
                 not getattr(action, "permission", None)
@@ -60,6 +70,10 @@ class ObjectActionsMixin:
         )
 
     def _get_object_actions_before_saving(self, request, obj):
+        if obj is None:
+            return (
+                []
+            )  # no object actions before saving for new objects, as the objects don't yet exist
         return filter(
             lambda x: not x["perform_after_saving"],
             self.get_object_actions(request, obj),
@@ -75,7 +89,7 @@ class ObjectActionsMixin:
         for action in self._get_object_actions_before_saving(request, obj):
             if request.POST.get(action["parameter_name"]) and not action["disabled"]:
                 response = self.perform_object_action(action, request, obj)
-                if response:
+                if response and issubclass(response.__class__, HttpResponse):
                     return response
 
         return super().changeform_view(request, object_id, form_url, extra_context)
@@ -84,8 +98,10 @@ class ObjectActionsMixin:
         for action in self._get_object_actions_after_saving(request, obj):
             if request.POST.get(action["parameter_name"]) and not action["disabled"]:
                 response = self.perform_object_action(action, request, obj)
-                if response:
-                    return response
+                if response and issubclass(response.__class__, HttpResponse):
+                    return response  # return to the return value of the action
+                else:
+                    return redirect(request.path)  # return to the same page
 
         return super().response_change(request, obj)
 
@@ -103,3 +119,82 @@ class ObjectActionsMixin:
             self.object_actions_after_related_objects, request, obj
         )
         return super().render_change_form(request, context, add, change, form_url, obj)
+
+    def _get_queryset_object_actions(self, actions, request):
+        for action_name in actions:
+            if callable(action_name):
+                action = action_name
+            else:
+                if not hasattr(self, action_name) or not callable(
+                    getattr(self, action_name)
+                ):
+                    continue
+                action = getattr(self, action_name)
+
+            if not action.include_in_queryset_actions:
+                continue
+
+            has_permission = bool(
+                not getattr(action, "permission", None)
+                or request.user.has_perm(getattr(action, "permission"))
+            )
+
+            if has_permission:
+                yield {
+                    "func": action.func_queryset,
+                    "short_description": action.label,
+                    "name": action.parameter_name,
+                }
+
+    def get_queryset_object_actions(self, request):
+        return self._get_queryset_object_actions(
+            self._get_all_object_actions(), request
+        )
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        object_actions = {
+            action["name"]: (
+                action["func"],
+                action["name"],
+                action["short_description"],
+            )
+            for action in self.get_queryset_object_actions(request)
+        }
+        return actions | object_actions
+
+    def perform_object_action_on_queryset(self, action, request, queryset):
+        if hasattr(action, "permission") and not request.user.has_perm(
+            action.permission
+        ):
+            return
+
+        count = 0
+
+        for obj in queryset:
+            if (
+                hasattr(action, "condition")
+                and callable(action.condition)
+                and not action.condition(request, obj)
+            ):
+                continue
+
+            if hasattr(action, "log_message"):
+                self.log_change(request, obj, action.log_message)
+
+            return_value = action(self, request, obj)
+
+            if bool(return_value):
+                count += 1
+
+        if hasattr(action, "after_queryset_action_callable") and callable(
+            action.after_queryset_action_callable
+        ):
+            action.after_queryset_action_callable(request, queryset, count)
+        else:
+            msg = gettext("Applied '%(action_message)s' on %(count)s %(name)s.") % {
+                "action_message": action.label,
+                "count": count,
+                "name": model_ngettext(self.opts, count),
+            }
+            self.message_user(request, msg, messages.SUCCESS)
